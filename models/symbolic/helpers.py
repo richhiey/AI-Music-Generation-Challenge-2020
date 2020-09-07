83181720f9a0b592b06b9db8d2af26759aa47568
Richhiey <richhiey.thomas@gmail.com>
2020-09-07 02:58:02 +0200

    Add initial code for FolkTransformer

--- models/symbolic/helpers.py
+++ models/symbolic/helpers.py
@@ -25,12 +25,10 @@ def positional_encoding(position, d_model):
                           d_model)
     # apply sin to even indices in the array; 2i
     angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
-  
     # apply cos to odd indices in the array; 2i+1
     angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
-    
+
     pos_encoding = angle_rads[np.newaxis, ...]
-    
     return tf.cast(pos_encoding, dtype=tf.float32)
 ## -------------------------------------------------------------------
 
--- models/symbolic/transformer.py
+++ models/symbolic/transformer.py
@@ -1,18 +1,70 @@
 import tensorflow as tf
 from .helpers import TransformerEncoder, TransformerDecoder
 
+class FolkTransformer(tf.keras.Model):
 
-class ABCTransformer(tf.keras.Model):
+    def __init__(self, model_configs, data_dimensions)
+        self.data_dimensions = data_dimensions
+        self.model_path = model_path
 
-    def __init__(self, num_layers, d_model, num_heads, dff, vocab_size, pe_target, rate=0.1):
+        self.tensorboard_logdir = os.path.join(
+            model_path,
+            'tensorboard',
+            'run'+datetime.now().strftime("%Y%m%d-%H%M%S")
+        )
+        self.file_writer = tf.summary.create_file_writer(
+            os.path.join(self.tensorboard_logdir, 'metrics')
+        )
+        self.file_writer.set_as_default()
+
+        model_config_path = os.path.join(model_path, 'transformer.json')
+        print(model_config_path)
+        if os.path.exists(model_config_path):
+            with open(model_config_path, 'r') as fp:
+                self.model_configs = json.load(fp)
+                print(self.model_configs)  
+
+        tokenizer_path = os.path.join(self.model_path, 'tunes_vocab.json')
+        if os.path.exists(tokenizer_path):
+            with open(tokenizer_path, 'r') as fp:
+                self.tunes_tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(json.load(fp))
+
+        self.model = Transformer(model_configs, data_dimensions)
+        self.loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+        self.ckpt = tf.train.Checkpoint(
+            step = tf.Variable(1),
+            optimizer = self.optimizer,
+            net = self.model
+        )
+        self.ckpt_manager = tf.train.CheckpointManager(
+            self.ckpt, 
+            os.path.join(self.model_path, 'ckpt'),
+            max_to_keep = 3
+        )
+
+
+    def train(self, dataset):
+        pass
+
+
+class Transformer(tf.keras.Model):
+
+    def __init__(self, configs, data_dimensions):
         super(Transformer, self).__init__()
-        self.decoder = TransformerDecoder(num_layers, d_model, num_heads, dff, 
-                           target_vocab_size, pe_target, rate)
+        self.decoder = TransformerDecoder(
+            configs["num_layers"],
+            configs["d_model"],
+            configs["num_heads"],
+            configs["dff"],
+            configs["target_vocab_size"],
+            configs["pe_target"],
+            configs["rate"]
+        )
         self.final_layer = tf.keras.layers.Dense(target_vocab_size)
 
 
-    def call(self, inp, tar, training, enc_padding_mask, 
-           look_ahead_mask, dec_padding_mask):
+    def call(self, inp, tar, training, look_ahead_mask):
 
         enc_output = self.encoder(inp, training, enc_padding_mask)  # (batch_size, inp_seq_len, d_model)
         # dec_output.shape == (batch_size, tar_seq_len, d_model)
